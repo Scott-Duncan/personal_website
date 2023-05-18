c5c6666eb5f7ff3ce0b89fd98f62077d951564ef
Scott Duncan <scott@kisutech.com>
2023-05-18 14:25:38 +0100

    Reverted back one version of rocket and added a post request route.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
+#![feature(decl_macro)]
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+use rocket::response::content::Json;
+use rocket::request::Form;
+
+#[derive(FromForm, Debug)]
+struct Book {
+  title: String,
+  author: String,
+  isbn: String
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+#[post("/book", data = "<book_form>")]
+fn new_book(book_form: Form<Book>) -> String {
+    let book: Book = book_form.into_inner();
+    let mut dummy_db: Vec<Book> = Vec::new();
+    dummy_db.push(book);
+    format!("Book added successfully: {:?}", dummy_db)
+}
+
+#[get("/hello")]
+fn hello() -> Json<&'static str> {
+    Json("{
+        'status': 'success',
+        'message': 'Hello API!'
+    }")
+}
+
+fn main() {
+    rocket::ignite()
+      .mount("/api", routes![hello])
+      .launch();
 }
